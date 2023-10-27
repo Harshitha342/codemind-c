18bd4fa3e5e2f58a7a566c158254e7ffba93a95e
Harshitha342 <147472791+Harshitha342@users.noreply.github.com>
2023-10-27 12:15:22 +0530

    #include<stdio.h> int main(){     int a;     scanf("%d",&a);     if(a%3==0 && a%5==0 && a%7==0){         printf("PlingPlangPlong");     }     else if(a%3==0 && a%5==0){         printf("PlingPlang");     }     else if(a%3==0 && a%7==0){         printf("PlingPlong");     }     else if(a%3==0){         printf("Pling");     }     else if(a%5==0){         printf("Plang");     }     else if(a%7==0){         printf("Plong");     }     else{         printf("%d",a);     } }

---
+++ RainDrop.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    if(a%3==0 && a%5==0 && a%7==0){
+        printf("PlingPlangPlong");
+    }
+    else if(a%3==0 && a%5==0){
+        printf("PlingPlang");
+    }
+    else if(a%3==0 && a%7==0){
+        printf("PlingPlong");
+    }
+    else if(a%3==0){
+        printf("Pling");
+    }
+    else if(a%5==0){
+        printf("Plang");
+    }
+    else if(a%7==0){
+        printf("Plong");
+    }
+    else{
+        printf("%d",a);
+    }
+}
