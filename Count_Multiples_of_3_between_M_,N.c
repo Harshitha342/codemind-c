b0bac356fa0e628903a37c9f4e1ff542ed644d53
Harshitha342 <147472791+Harshitha342@users.noreply.github.com>
2023-12-12 10:54:17 +0530

    #include<stdio.h> int main(){     int a,b;     scanf("%d %d",&a,&b);     int cnt=0;     for(int i=a ; i<=b ; i++){         if(i%3==0){             cnt++;         }     }     printf("%d",cnt); }

---
+++ Count_Multiples_of_3_between_M_,N.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    int cnt=0;
+    for(int i=a ; i<=b ; i++){
+        if(i%3==0){
+            cnt++;
+        }
+    }
+    printf("%d",cnt);
+}
