44333c3980128c8d20ebf3337d259e55f336984c
Harshitha342 <147472791+Harshitha342@users.noreply.github.com>
2024-01-02 20:10:57 +0530

    #include <stdio.h>
      int main () {
          int num;
          scanf("%d", &num);
          
          int sum = 0;
          for (int i = 1; i <= num/2; i++) { // excluding the number itself.
              if (num % i == 0) {
                  sum += i;
              }
          }
          printf("%d", sum);
      }

--- _Sum_of_factors.c
+++ _Sum_of_factors.c
@@ -1,12 +1,14 @@
-#include<stdio.h>
-int main(){
-    int num;
-    scanf("%d",&num);
-    int fac=0;
-    for(int i=1; i<num ; i++){
-        if(num%i==0){
-           fac=fac+i;
-        }
-    }
-    printf("%d",fac);
-}
+#include <stdio.h>
+
+  int main () {
+      int num;
+      scanf("%d", &num);
+      
+      int sum = 0;
+      for (int i = 1; i <= num/2; i++) { // excluding the number itself.
+          if (num % i == 0) {
+              sum += i;
+          }
+      }
+      printf("%d", sum);
+  }
