c70a388db2b1f01403f78420c1808f3bfc5fcc46
Harshitha <147472791+Harshitha342@users.noreply.github.com>
2024-01-04 00:48:54 +0530

    #include <stdio.h> #include <math.h>
      int main () {
          int a, b;
          scanf("%d %d", &a, &b);
          
          float sum = 0;
          for (int i = a; i <= b; i++) {
              sum += sqrt(i);
          }
          printf("%.2f", sum);
          
      }

---
+++ Sum_of_Square_Roots.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+  int main () {
+      int a, b;
+      scanf("%d %d", &a, &b);
+      
+      float sum = 0;
+      for (int i = a; i <= b; i++) {
+          sum += sqrt(i);
+      }
+      printf("%.2f", sum);
+      
+  }
