19a6fe5090d9ca4da74839d65034c53e3aa59d00
Harshitha <147472791+Harshitha342@users.noreply.github.com>
2024-01-03 22:41:48 +0530

    #include <stdio.h> #include <ctype.h>
     int main () {
        char str[1001];
        scanf("%s", str);
        
        int sum = 0;
        for (int i = 0; str[i] != NULL; i++) {
            if (isdigit(str[i])) {
                sum += str[i] - '0';
            }
        }
        printf("%d", sum);
     }

---
+++ _Sum_of_all_numbers_present_in_the_String.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+
+ int main () {
+    char str[1001];
+    scanf("%s", str);
+    
+    int sum = 0;
+    for (int i = 0; str[i] != NULL; i++) {
+        if (isdigit(str[i])) {
+            sum += str[i] - '0';
+        }
+    }
+    printf("%d", sum);
+ }
