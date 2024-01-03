b74a0bf992690303d5f5622f9ed401a4728dca1e
Harshitha <147472791+Harshitha342@users.noreply.github.com>
2024-01-04 00:53:25 +0530

    #include <stdio.h> #include <math.h>
    int isprime (int n) {
        if (n < 2) {
            return 0;
        }
        
        if (n < 4) {
            return 1;
        }
        
        if (n % 2 == 0 || n % 3 == 0) return 0;
        
        for (int i = 2; i <= sqrt(n); i++) {
            if (n % i == 0) {
                return 0;
            }
        }
        return 1;
    }
    
     int main () {
         int num;
         scanf("%d", &num);
         
         if (isprime(num)) {
             printf("Prime");
         } else {
             printf("Not Prime");
         }
         
     }

--- Prime.c
+++ Prime.c
@@ -1,17 +1,33 @@
-#include<stdio.h>
-int main(){
-    int num;
-    scanf("%d",&num);
-    int fac_cnt=0;
-    for(int i = 1 ; i <= num ; i++){
-        if(num % i == 0){
-            fac_cnt++;
+#include <stdio.h>
+#include <math.h>
+
+int isprime (int n) {
+    if (n < 2) {
+        return 0;
+    }
+    
+    if (n < 4) {
+        return 1;
+    }
+    
+    if (n % 2 == 0 || n % 3 == 0) return 0;
+    
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            return 0;
         }
-       }
-       if(fac_cnt == 2){
-        printf("Prime");
-       }
-       else{
-        printf("Not Prime");
-       }
+    }
+    return 1;
 }
+
+ int main () {
+     int num;
+     scanf("%d", &num);
+     
+     if (isprime(num)) {
+         printf("Prime");
+     } else {
+         printf("Not Prime");
+     }
+     
+ }
