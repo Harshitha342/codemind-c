09a1a14108045017a84983a9087af143f5343e0e
Harshitha <147472791+Harshitha342@users.noreply.github.com>
2024-01-08 21:55:16 +0530

    #include<stdio.h>
       int main () {
           float salary, hra, da;
           scanf("%f %f %f", &salary, &hra, &da);
           printf("%.2f
    %.2f", 0.12 * salary, salary + hra + da + (0.12*salary));
           return 0;
       }

---
+++ Calculate_Gross_Salary_of_an_employee.c
@@ -0,0 +1,9 @@
+#include<stdio.h> 
+
+   int main () {
+       float salary, hra, da;
+       scanf("%f %f %f", &salary, &hra, &da);
+       printf("%.2f
+%.2f", 0.12 * salary, salary + hra + da + (0.12*salary));
+       return 0;
+   }
