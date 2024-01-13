62c2aecedf9e172604520baa71998ecec3fdb861
Harshitha <147472791+Harshitha342@users.noreply.github.com>
2024-01-13 19:34:12 +0530

    #include <stdio.h> #include <ctype.h>
      int main () {
          char str[1001];
          scanf("%[^
    ]", str);
          
          int Len = 0;
          for (int i = 0; str[i] != NULL; i++) {
              if (islower(str[i])) {
                  Len++;
              }
          }
          printf("%d", Len);
      }

---
+++ Lowercase_Letters_in_a_String.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+
+  int main () {
+      char str[1001];
+      scanf("%[^
+]", str);
+      
+      int Len = 0;
+      for (int i = 0; str[i] != NULL; i++) {
+          if (islower(str[i])) {
+              Len++;
+          }
+      }
+      printf("%d", Len);
+  }
