82c86c5cbd137ccaef7a97eb02a6d40346b86dba
Harshitha <147472791+Harshitha342@users.noreply.github.com>
2024-01-15 13:06:40 +0530

    #include <stdio.h>
    void mod (int a, int b) {
        printf("%d", a % b);
    }
    
    int main () {
        int a, b;
        scanf("%d %d", &a, &b);
        mod(a, b);
    }

--- Modulus.c
+++ Modulus.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-  int main () {
-      int a, b;
-      scanf("%d %d", &a, &b);
-      printf("%d", a % b);
-  }
+void mod (int a, int b) {
+    printf("%d", a % b);
+}
+
+int main () {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    mod(a, b);
+}
