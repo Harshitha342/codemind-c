e4d0f49185ccda9fcb263e5e4b6a314a6f1d4709
Harshitha <147472791+Harshitha342@users.noreply.github.com>
2024-01-20 03:22:47 +0530

    #include <stdio.h>
    int maximumChocolates (int a, int b, int c) {
        return ((5 * a) + (10 * b)) / c;
    }
    
    void Display (int max) {
        printf("%d", max);
    }
    
    void read () {
        int x, y, z;
        scanf("%d %d %d", &x, &y, &z);
        Display (maximumChocolates(x, y, z));
    }
    
    int main () {
        read();
    }

--- Romeo_and_Juliet.c
+++ Romeo_and_Juliet.c
@@ -1,8 +1,19 @@
-#include<stdio.h> 
+#include <stdio.h>
 
-   int main () {
-       int x, y, z;
-       scanf("%d %d %d", &x, &y, &z);
-       printf("%d", (5 * x + 10 * y) / z);
-       return 0;
-   }
+int maximumChocolates (int a, int b, int c) {
+    return ((5 * a) + (10 * b)) / c;
+}
+
+void Display (int max) {
+    printf("%d", max);
+}
+
+void read () {
+    int x, y, z;
+    scanf("%d %d %d", &x, &y, &z);
+    Display (maximumChocolates(x, y, z));
+}
+
+int main () {
+    read();
+}
