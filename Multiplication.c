f805d534782053f0520e72f5e5a4cbf316256575
Harshitha <147472791+Harshitha342@users.noreply.github.com>
2024-01-19 14:12:35 +0530

    #include <stdio.h>
    void read();
    void mul (int n);
    void print(int n);
    
    int main () {
        read();
    }
    
    void print (int n) {
        printf("%d", n);
    }
    
    void mul (int *a, int *b, int *c) {
        (*c) = (*a) * (*b);
    }
    
    void read () {
        int a, b;
        scanf("%d %d", &a, &b);
        int c;
        mul (&a, &b, &c);
        print(c);
    }

--- Multiplication.c
+++ Multiplication.c
@@ -1,7 +1,25 @@
-#include<stdio.h> 
+#include <stdio.h>
 
-  int main () {
-      int x,y;
-      scanf("%d %d", &x ,&y);
-      printf("%d", x * y);
-  }
+void read();
+void mul (int n);
+void print(int n);
+
+int main () {
+    read();
+}
+
+void print (int n) {
+    printf("%d", n);
+}
+
+void mul (int *a, int *b, int *c) {
+    (*c) = (*a) * (*b);
+}
+
+void read () {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int c;
+    mul (&a, &b, &c);
+    print(c);
+}
