8b418cdf44462cc8f7e3cbc5ae128ca4085592a2
Harshitha <147472791+Harshitha342@users.noreply.github.com>
2024-01-22 20:51:27 +0530

    #include <stdio.h>
    void Display(int area);
    
    int AreaofTriangle (int b, int h) {
        return (b * h) / 2;
    }
    
    int main(void) {
        int b, h;
        scanf("%d %d", &b, &h);
        Display(AreaofTriangle(b, h));
    }
    
    void Display(int area) {
        printf("%d", area);
    }

--- Area_of_a_triangle_.c
+++ Area_of_a_triangle_.c
@@ -1,8 +1,17 @@
-#include<stdio.h>
-  int main ()
-  {
-      int b,h;
-      scanf("%d %d", &b, &h);
-      printf("%d", (b * h) / 2);
-      return 0;
-  }
+#include <stdio.h>
+
+void Display(int area);
+
+int AreaofTriangle (int b, int h) {
+    return (b * h) / 2;
+}
+
+int main(void) {
+    int b, h;
+    scanf("%d %d", &b, &h);
+    Display(AreaofTriangle(b, h));
+}
+
+void Display(int area) {
+    printf("%d", area);
+}
