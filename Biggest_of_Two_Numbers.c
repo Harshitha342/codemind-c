ff7d9be1c99ccffdf1020ed9b4b6920a852fb81f
Harshitha <147472791+Harshitha342@users.noreply.github.com>
2024-01-25 22:02:13 +0530

    #include <stdio.h>
    int max (int a, int b) {
        return (a > b)? a : b;
    }
    
    int main() {
        int a, b;
        scanf("%d %d", &a, &b);
        printf("%d", max(a, b));
    }

--- Biggest_of_Two_Numbers.c
+++ Biggest_of_Two_Numbers.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
-int main(){
-    int a,b;
-    scanf("%d %d",&a,&b);
-    if (a>b){
-        printf("%d",a);
-    }
-    else {
-        printf("%d",b);
-    }
+#include <stdio.h>
+
+int max (int a, int b) {
+    return (a > b)? a : b;
+}
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", max(a, b));
 }
