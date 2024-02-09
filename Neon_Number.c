ae2156934460780dfb0a585e2cca3824b5aba37a
Harshitha <147472791+Harshitha342@users.noreply.github.com>
2024-02-09 22:34:34 +0530

    #include <stdio.h> #include <math.h>
    int isneon(int num) {
        int power = pow(num, 2);
        int pow_sum = 0;
        
        while(power != 0) {
            pow_sum += power % 10;
            power /= 10;
        }
        
        return (pow_sum == num);
    }
    
    int main() {
        int num;
        scanf("%d", &num);
        
        if(isneon(num)) {
            printf("Neon Number");
        } else {
            printf("Not Neon Number");
        }
        
    }

--- Neon_Number.c
+++ Neon_Number.c
@@ -1,18 +1,26 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+#include <math.h>
+
+int isneon(int num) {
+    int power = pow(num, 2);
+    int pow_sum = 0;
+    
+    while(power != 0) {
+        pow_sum += power % 10;
+        power /= 10;
+    }
+    
+    return (pow_sum == num);
+}
+
+int main() {
     int num;
-    scanf("%d",&num);
-    int s = num*num;
-    int last,sum=0;
-    while(s){
-        last = s%10;
-        sum = sum+last;
-        s=s/10;
-    }
-    if(sum == num){
+    scanf("%d", &num);
+    
+    if(isneon(num)) {
         printf("Neon Number");
-    }
-    else{
+    } else {
         printf("Not Neon Number");
     }
+    
 }
