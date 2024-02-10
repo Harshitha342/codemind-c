0a9d98b3dbc3ac6bab24c2532a674d54918cbd61
Harshitha <147472791+Harshitha342@users.noreply.github.com>
2024-02-10 23:20:40 +0530

    #include <stdio.h> #include <math.h>
    int isprime(int num) {
        if(num <= 1) {
            return 0;
        } else if (num <= 3) {
            return 1;
        } else if (num % 2 == 0 || num % 3 == 0) {
            return 0;
        } else {
            for (int i = 2; i <= sqrt(num); i++) {
                if(num % i == 0) {
                    return 0;
                }
            }
        }
        return 1;
    }
    
    int main () {
        int num;
        scanf("%d", &num);
        
        if(isprime(num)) {
            printf("Prime");
        } else {
            printf("Not Prime");
        }
        
        return 0;
    }

--- Prime.c
+++ Prime.c
@@ -1,35 +1,32 @@
 #include <stdio.h>
 #include <math.h>
 
-int isprime (int n) {
-    if (n < 2) {
+int isprime(int num) {
+    if(num <= 1) {
         return 0;
-    }
-    
-    if (n < 4) {
+    } else if (num <= 3) {
         return 1;
-    }
-    
-    if (n % 2 == 0 || n % 3 == 0) {
+    } else if (num % 2 == 0 || num % 3 == 0) {
         return 0;
-    }
-    
-    for (int i = 2; i <= sqrt(n); i++) {
-        if (n % i == 0) {
-            return 0;
+    } else {
+        for (int i = 2; i <= sqrt(num); i++) {
+            if(num % i == 0) {
+                return 0;
+            }
         }
     }
     return 1;
 }
 
- int main () {
-     int num;
-     scanf("%d", &num);
-     
-     if (isprime(num)) {
-         printf("Prime");
-     } else {
-         printf("Not Prime");
-     }
-     
- }
+int main () {
+    int num;
+    scanf("%d", &num);
+    
+    if(isprime(num)) {
+        printf("Prime");
+    } else {
+        printf("Not Prime");
+    }
+    
+    return 0;
+}
